a8f4719eabfe30e6f842ad8353bf3a06789be376
Liams <Liams@DESKTOP-BU464IC>
2023-06-30 11:13:45 +0200

    generic repo

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -10,24 +10,42 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductRepository repo;
+        private readonly IGenericRepository<Product> productRepo;
+        private readonly IGenericRepository<ProductType> typeRepo;
+        private readonly IGenericRepository<ProductBrand> brandRepo;
 
-        public ProductsController(IProductRepository repo)
+        public ProductsController(IGenericRepository<Product> productRepo, IGenericRepository<ProductType> typeRepo, IGenericRepository<ProductBrand> brandRepo)
         {
-            this.repo = repo;
+            this.productRepo = productRepo;
+            this.typeRepo = typeRepo;
+            this.brandRepo = brandRepo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProduct()
         {
-            var products = await repo.GetAsync();
+            var products = await productRepo.GetAllAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await repo.GetAsync(id);
+            return await productRepo.GetByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<Product>>> GetBrands()
+        {
+            var products = await brandRepo.GetAllAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<List<Product>>> GetTypes()
+        {
+            var products = await typeRepo.GetAllAsync();
+            return Ok(products);
         }
     }
 }
